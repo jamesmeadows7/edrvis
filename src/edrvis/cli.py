672ea6f7acf8061a0e55cffae141b01064484abd
jamesmeadows7 <james.w.meadows@durham.ac.uk>
2026-04-28 17:02:03 +0100

    feat: add func to read edr files

--- src/edrvis/cli.py
+++ src/edrvis/cli.py
@@ -1,6 +1,24 @@
+"""Command-line entry point for `edrvis`."""
+
+import argparse
+from pathlib import Path
+
+from edrvis.reader import read_edr
+
+
 def main():
     """Entry point for the `edrvis` command."""
-    from edrvis.app import BasicApp
+    parser = argparse.ArgumentParser(
+        prog="edrvis", description="Visualise GROMACS edr files in the terminal."
+    )
+    parser.add_argument("edr_file", type=Path, help="edr file path")
+    args = parser.parse_args()
 
-    app = BasicApp()
-    app.run()
+    edr_path: Path = args.edr_file
+
+    read_edr(edr_path)
+
+    # from edrvis.app import BasicApp
+
+    # app = BasicApp()
+    # app.run()
---
+++ src/edrvis/reader.py
@@ -0,0 +1,16 @@
+"""edr file reading"""
+
+from pathlib import Path
+
+import pyedr
+from numpy import ndarray
+
+
+def read_edr(edr_path: Path) -> tuple[dict[str, ndarray], dict[str, str]]:
+    if not edr_path.exists():
+        raise FileNotFoundError(f"edr file not found at {edr_path}.")
+
+    edr_data: dict[str, ndarray] = pyedr.edr_to_dict(str(edr_path))
+    edr_units: dict[str, str] = pyedr.get_unit_dictionary(str(edr_path))
+
+    return edr_data, edr_units
